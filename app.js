5533ee4003485850df5eca966c381fb6f2b9a37a
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-04-27 12:42:40 +0530

    uploaded correct content

--- app.js
+++ app.js
@@ -1,19 +1,71 @@
-// Declare and define Object with nested Object in literal notation and create.
-var employee = {
-  name: 'Rajendhar Mamidala',
-  age : 31,
-  salary: 10000,
+// Employe A Object with liter notation.
+var empA = {
+  name: 'Rajendhar M',
+  age : 30,
+  salary: 11000,
   address: {
-    city: 'Mumbai',
-    state: 'Maharashtra',
+    city: 'Mumbai1',
+    state: 'Maharashtra1',
     pincode: '400708',
   }
 }
 
-// Print object properties and nested Object properties in console.
-console.log(employee.name);
-console.log(employee.age);
-console.log(employee.salary);
-console.log(employee.address.city);
-console.log(employee.address.state);
-console.log(employee.address.pincode);
+// Employe B Object with liter notation.
+var empB = {
+  name: 'Rajendhar Ma',
+  age : 31,
+  salary: 12000,
+  address: {
+    city: 'Mumbai2',
+    state: 'Maharashtra2',
+    pincode: '400704',
+  }
+}
+
+// Employe C Object with liter notation.
+var empC = {
+  name: 'Rajendhar Mam',
+  age : 32,
+  salary: 13000,
+  address: {
+    city: 'Mumbai3',
+    state: 'Maharashtra3',
+    pincode: '400703',
+  }
+}
+
+// Employe D Object with liter notation.
+var empD = {
+  name: 'Rajendhar Mami',
+  age : 33,
+  salary: 14000,
+  address: {
+    city: 'Mumbai4',
+    state: 'Maharashtra4',
+    pincode: '400702',
+  }
+}
+
+// Employe E Object with liter notation.
+var empE = {
+  name: 'Rajendhar Mamid',
+  age : 34,
+  salary: 15000,
+  address: {
+    city: 'Mumbai5',
+    state: 'Maharashtra5',
+    pincode: '400701',
+  }
+}
+
+// Array to store employee objects.
+var empArray = [empA, empB, empC, empD, empE];
+
+// Print empC properties in console,
+console.log(empArray[2].name);
+console.log(empArray[2].age);
+console.log(empArray[2].salary);
+console.log(empArray[2].address.state);
+console.log(empArray[2].address.city);
+console.log(empArray[2].address.pincode);
+
